e1ea6c7598390e6322e8f4e63a4b2e70a8934aaf
KomalHonmane <129865883+KomalHonmane@users.noreply.github.com>
2023-04-04 19:09:49 +0530

    MAUI_MobileApp

---
+++ MobileApp_MAUI/Classes/ImagePrediction.cs
@@ -0,0 +1,139 @@
+﻿using Microsoft.Azure.CognitiveServices.Vision.CustomVision.Prediction.Models;
+using Microsoft.Azure.CognitiveServices.Vision.CustomVision.Prediction;
+using Microsoft.Maui.Graphics.Platform;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiMedia.Classes
+{
+    public class ImagePrediction
+    {
+
+        #region obsolete
+        private const int ImageMaxSizeBytes = 4194304;
+        private const int ImageMaxResolution = 1024;
+
+        public static bool IsRunning { get; private set; }
+
+        public static async Task<PredictionModel> ClassifyImage(Stream photoStream)
+        {
+            try
+            {
+                IsRunning = true;
+
+                var endpoint = new CustomVisionPredictionClient(new ApiKeyServiceClientCredentials("65db0bfd444548aeb67e875d6a04324f"))
+                {
+                    Endpoint = "https://customimageprediction.cognitiveservices.azure.com/"
+                };
+
+
+                // Send image to the Custom Vision API
+                var results = await endpoint.ClassifyImageAsync(Guid.Parse("8ccc7d4e-48f8-463a-a10c-599fb64644b4"), "Flowers", photoStream).ConfigureAwait(false);
+
+                // Return the most likely prediction
+                return results.Predictions?.OrderByDescending(x => x.Probability).FirstOrDefault();
+            }
+            catch (Exception ex)
+            {
+                return new PredictionModel();
+            }
+            finally
+            {
+                IsRunning = false;
+            }
+        }
+
+        #endregion
+
+        public static async Task<byte[]> ResizePhotoStream(FileResult photo)
+        {
+            byte[] result = null;
+
+            using (var stream = await photo.OpenReadAsync())
+            {
+                if (stream.Length > ImageMaxSizeBytes)
+                {
+                    var image = PlatformImage.FromStream(stream);
+                    if (image != null)
+                    {
+                        var newImage = image.Downsize(ImageMaxResolution, true);
+                        result = newImage.AsBytes();
+                    }
+                }
+                else
+                {
+                    using (var binaryReader = new BinaryReader(stream))
+                    {
+                        result = binaryReader.ReadBytes((int)stream.Length);
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        public static async Task<PayloadPrediction> MakePredictionRequest(byte[] imageByteArray)
+        {
+            try
+            {
+                var client = new HttpClient();
+                var jsonResponse = string.Empty;
+                // Request headers - replace with your valid subscription key. 
+                client.DefaultRequestHeaders.Add("prediction-key", "a72c0b9983a742479281d5fb401a1588");
+                // Prediction URL - replace with your valid prediction URL. 
+                string url = "https://customimageprediction-prediction.cognitiveservices.azure.com/customvision/v3.0/Prediction/8ccc7d4e-48f8-463a-a10c-599fb64644b4/classify/iterations/Flowers_Iteration1/image";
+                HttpResponseMessage response;
+                // Request body. Try this sample with a locally stored image. 
+                using (var content = new ByteArrayContent(imageByteArray))
+                {
+                    content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+                    response = await client.PostAsync(url, content).ConfigureAwait(false);
+                    jsonResponse = await response.Content.ReadAsStringAsync();
+                    RootObject deserializedRootOjbect = JsonConvert.DeserializeObject<RootObject>(jsonResponse);
+                    PayloadPrediction payloadPrediction = new PayloadPrediction(deserializedRootOjbect);                
+                    return payloadPrediction;
+                }
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+    }
+
+    public class Global
+    {
+        public string result;
+    }
+    public class Prediction
+    {
+        public string TagId { get; set; }
+        public string TagName { get; set; }
+        public double Probability { get; set; }
+    }
+    public class PayloadPrediction
+    {
+        public PayloadPrediction(RootObject rootObject)
+        {
+            this.IsFlower = (rootObject.Predictions[0].Probability > 0.7) ? true : false;
+            this.TagName = rootObject.Predictions[0].TagName;
+        }
+        public bool IsFlower { get; set; }
+        public string TagName { get; set; }
+    }
+    public class RootObject
+    {
+        public string Id { get; set; }
+        public string Project { get; set; }
+        public string Iteration { get; set; }
+        public DateTime Created { get; set; }
+        public List<Prediction> Predictions { get; set; }
+    }
+
+}
---
+++ MobileApp_MAUI/MainPage.xaml.cs
@@ -0,0 +1,91 @@
+﻿using MauiMedia.Classes;
+using Microsoft.Maui.Graphics.Platform;
+using Plugin.Media;
+using Plugin.Media.Abstractions;
+
+namespace MauiMedia;
+
+public partial class MainPage : ContentPage
+{
+    private int selectedCompressionQuality;
+
+    public MainPage()
+    {
+        InitializeComponent();        
+    }
+
+    public async Task<PermissionStatus> CheckAndRequestLocationPermission()
+    {
+        PermissionStatus status = await Permissions.CheckStatusAsync<Permissions.LocationWhenInUse>();
+
+        if (status == PermissionStatus.Granted)
+            return status;
+
+        if (status == PermissionStatus.Denied && DeviceInfo.Platform == DevicePlatform.iOS)
+        {
+            // Prompt the user to turn on in settings
+            // On iOS once a permission has been denied it may not be requested again from the application
+            return status;
+        }
+
+        if (Permissions.ShouldShowRationale<Permissions.LocationWhenInUse>())
+        {
+            // Prompt the user with additional information as to why the permission is needed
+        }
+
+        status = await Permissions.RequestAsync<Permissions.LocationWhenInUse>();
+
+        return status;
+    }
+
+    private async void OnPickPhotoClicked(object sender, EventArgs e)
+    {
+        await ProcessPhotoAsync(false);
+    }
+    private async Task ProcessPhotoAsync(bool useCamera)
+    {
+        //await CheckAndRequestLocationPermission();
+        //var photo = useCamera
+        //  ? await MediaPicker.Default.CapturePhotoAsync()
+        //  : await MediaPicker.Default.PickPhotoAsync();
+
+
+        //if (photo is { })
+        //{
+            UploadedOrSelectedImage.Source = "test_image.png";
+        FileSizeLabel.Text = "Either it is not a flower or it is not on the prediction list.";
+        //// Resize to allowed size - 4MB
+        //var resizedPhoto = await ImagePrediction.ResizePhotoStream(photo);
+        //var result1 = ImagePrediction.MakePredictionRequest(resizedPhoto).Result;
+
+        //if(result1.IsFlower)
+        //{
+        //    FileSizeLabel.Text = $"It's a{result1.TagName.ToString().ToUpper()}";
+        //}
+        //else
+        //{
+        //    FileSizeLabel.Text = "Either it is not a flower or it is not on the prediction list.";
+        //}
+
+        //}
+        //else
+        //{
+        //    FileSizeLabel.Text = "";
+        //}
+    }
+
+
+    private async void OnTakePhotoClicked(object sender, EventArgs e)
+    {
+        await ProcessPhotoAsync(true);
+    }
+
+    private void Slider_DragCompleted(object sender, EventArgs e)
+    {
+        selectedCompressionQuality = (int)((sender as Slider).Value * 100);
+        PredictionLabel.Text = $"Prediction: {selectedCompressionQuality}";
+    }
+
+
+}
+
